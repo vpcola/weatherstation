3530d1a72bef45c0a6ebad73d35c92d6c6c001dc
Vergil Cola <vpcola@gmail.com>
2019-06-06 17:35:55 +0800

    Added dust sensor component

---
+++ components/dustsensor/dustsensor.h
@@ -0,0 +1,18 @@
+#ifndef _DUST_SENSOR_H_
+#define _DUST_SENSOR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void dustsensor_init();
+float dustsensor_read();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
